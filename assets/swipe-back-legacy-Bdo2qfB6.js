a201f1a5ad95eb6c7952f63171d5c3a06953a8df
clieford13 <cliefordrule21@gmail.com>
2025-03-14 11:29:50 +0800

    Updates

--- assets/swipe-back-legacy-Bdo2qfB6.js
+++ assets/swipe-back-legacy-Bdo2qfB6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BUeiWagc.js","./index3-legacy-DYSrmzka.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-DGqHWb5v.js","./index3-legacy-BFBWVhLM.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
