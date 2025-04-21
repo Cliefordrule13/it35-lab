b21bceaf40ffb21ac47a575b67513ae28e246d3c
cliefordrule13 <cliefordrule13@gmail.com>
2025-04-21 20:20:44 +0800

    update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,28 +1,33 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const About: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Page2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default About;
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  IonAvatar,
+  IonImg
+} from '@ionic/react';
+
+const About: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>About</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className="ion-padding" style={{ textAlign: 'center' }}>
+        <IonAvatar style={{ margin: '0 auto', width: '100px', height: '100px' }}>
+          <IonImg src="https://www.gravatar.com/avatar?d=mp" alt="Avatar" />
+        </IonAvatar>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default About;
