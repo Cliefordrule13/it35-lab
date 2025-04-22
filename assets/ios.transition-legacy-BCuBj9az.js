d574878934a07d57e6707505020803cb4ad896bb
cliefordrule13 <cliefordrule13@gmail.com>
2025-04-22 11:46:29 +0800

    Updates

--- assets/ios.transition-legacy-BCuBj9az.js
+++ assets/ios.transition-legacy-BCuBj9az.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CJfDTlwR.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BNvrEsQV.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
