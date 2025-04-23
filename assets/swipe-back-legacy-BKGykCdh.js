a7c24a157de868813245620cbb3ace6adb270539
cliefordrule13 <cliefordrule13@gmail.com>
2025-04-23 09:21:17 +0800

    Updates

--- assets/swipe-back-legacy-BKGykCdh.js
+++ assets/swipe-back-legacy-BKGykCdh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DdF_Iyoz.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CIXlqMrD.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
